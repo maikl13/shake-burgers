5f2d2adb58fdb91fa48bf3b9d910fe0af61c91b8
maikl13 <maiklalana@gmail.com>
2021-11-25 22:39:15 +0400

    1- setup the app , set colors

---
+++ app/src/main/java/com/shake/shake_burgers/MainActivity.java
@@ -0,0 +1,14 @@
+package com.shake.shake_burgers;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
